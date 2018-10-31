63d7bcac760b1acb1a92419f0c6d95832148e2df
Tareq Anwar <trqnwr@gmail.com>
2018-10-31 02:39:46 -0500

    moved  attributes to currect dom element

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Header, Container, Divider, Icon } from 'semantic-ui-react';
+
 import SearchBar from './SearchBar';
 
 const Layout = ({ children }) => {
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,10 @@ import { Grid, Search } from 'semantic-ui-react';
 
 const SearchBar = () => {
     return (
-        <Grid>
-            <Grid.Column centered columns={2}>
+        <Grid centered columns={2}>
+            <Grid.Column>
                 <Search 
-                    placeholder='Search...' 
+                    placeholder='Search...'
                 />
             </Grid.Column>
         </Grid>

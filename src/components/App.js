5c0522eb8080be296dd6f431cfc12164c732e8d0
Tareq Anwar <trqnwr@gmail.com>
2018-10-31 01:43:33 -0500

    resturctured compnents for youtube app

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,6 @@ import importedComponent from 'react-imported-component';
 import Home from './Home';
 import Loading from './Loading';
 
-const AsyncDynamicPAge = importedComponent(
-  () => import(/* webpackChunkName:'DynamicPage' */ './DynamicPage'),
-  {
-    LoadingComponent: Loading
-  }
-);
 const AsyncNoMatch = importedComponent(
   () => import(/* webpackChunkName:'NoMatch' */ './NoMatch'),
   {
@@ -24,7 +18,6 @@ const App = () => {
       <div>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/dynamic" component={AsyncDynamicPAge} />
           <Route component={AsyncNoMatch} />
         </Switch>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,23 +3,10 @@ import { Link } from 'react-router-dom';
 import { Grid } from 'semantic-ui-react';
 
 import Layout from './Layout';
-import Features from './Features';
-import Profile from './Profile';
 
 const Home = () => {
   return (
     <Layout>
-      <Grid stackable columns={2}>
-        <Grid.Row>
-          <Grid.Column>
-            <Features />
-            <Link to="/dynamic">Navigate to Dynamic Page</Link>
-          </Grid.Column>
-          <Grid.Column>
-            <Profile />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
     </Layout>
   );
 };

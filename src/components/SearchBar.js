3138941adf6908b2c09006cd45c6212f6ee885a9
Tareq Anwar <trqnwr@gmail.com>
2018-11-06 20:52:40 -0600

    converted into class based component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Grid, Input, Divider } from 'semantic-ui-react';
 
-import './SearchBar.css';
-
-const SearchBar = () => {
-    return (
-        <Grid centered columns={2}>
-            <Grid.Column>
-                <Divider hidden />
-                <Input 
-                    fluid
-                    icon='search'
-                    placeholder='Search...'
-                />
-                <Divider hidden />
-            </Grid.Column>
-        </Grid>
-    );
+class SearchBar extends Component {
+    render() {
+        return (
+            <Grid centered columns={2}>
+                <Grid.Column>
+                    <Divider hidden />
+                    <Input 
+                        fluid
+                        icon='search'
+                        placeholder='Search...'
+                    />
+                    <Divider hidden />
+                </Grid.Column>
+            </Grid>
+        );
+    }
 };
 
 export default SearchBar;

5fbdfeac3f7a824b424bddb9afd2b45051f92a49
Tareq Anwar <trqnwr@gmail.com>
2018-11-06 22:49:46 -0600

    added seletedVideo state and VideoList

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,34 @@ import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyBh5xn0vGLJSNaclSUm8gvEbtr2F2mJ2wY';
 
 import Layout from './Layout';
-import Loading from './Loading';
+import VideoList from './VideoList';
+import VideoDetails from './VideoDetails';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
     
     YTSearch({key: API_KEY, term: 'Tareq'}, videos => {
-      this.setState({ videos})
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render() {
     return (
       <div>
         <Layout>
+              <VideoDetails video={this.state.selectedVideo} />
+              <VideoList 
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} 
+              />
         </Layout>
       </div>
     );

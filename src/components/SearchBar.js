c0b634995946a7ea06652d5b41194d7d7c3cddc9
Tareq Anwar <trqnwr@gmail.com>
2018-11-06 21:13:08 -0600

    Added react state for search  term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { Grid, Input, Divider } from 'semantic-ui-react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: ''};
+    }
+
     render() {
         return (
             <Grid centered columns={2}>
@@ -10,6 +16,8 @@ class SearchBar extends Component {
                     <Input 
                         fluid
                         icon='search'
+                        value={this.state.term}
+                        onChange={event => this.setState({ term: event.target.value} )}
                         placeholder='Search...'
                     />
                     <Divider hidden />
